5d4ee14a95af8ad6fdc17aeca044553e0945f47a
zamfofex <zamfofex@twdb.moe>
2022-01-08 12:01:30 -0300

    update mold

--- zamfofex/compilers/mold.scm
+++ zamfofex/compilers/mold.scm
@@ -5,6 +5,8 @@
   (guix licenses)
   (guix git-download)
   (guix build-system gnu)
+  (guix build-system cmake)
+  (guix utils)
   (gnu packages tls)
   (gnu packages digest)
   (gnu packages tbb)
@@ -12,17 +14,37 @@
   (gnu packages compression)
   (gnu packages cmake))
 
+(define-public mimalloc
+  (package
+    (name "mimalloc")
+    (version "2.0.3")
+    (home-page "https://github.com/microsoft/mimalloc")
+    (synopsis "a compact general purpose allocator with excellent performance")
+    (description synopsis)
+    
+    (license expat)
+    
+    (source
+      (origin
+        (method git-fetch)
+        (uri
+          (git-reference
+            (url "https://github.com/microsoft/mimalloc")
+            (commit (string-append "v" version))))
+        (sha256 (base32 "0avamijn2dcrkba291rmdiv6z0kjzkhc1vigcwmhk9h0cvsc0q44"))))
+    (build-system cmake-build-system)))
+
 (define-public mold
   (package
     (name "mold")
-    (version "0.9.1")
+    (version "1.0.1")
     (home-page "https://github.com/rui314/mold")
     (synopsis "A Modern Linker")
     (description "mold is a high performance drop-in replacement for existing Unix linkers.")
     
     (license agpl3)
     
-    (inputs `(("openssl" ,openssl) ("xxhash" ,xxhash) ("tbb" ,tbb) ("zlib" ,zlib) ("gcc" ,gcc-11) ("cmake" ,cmake)))
+    (inputs (list openssl xxhash tbb zlib mimalloc))
     
     (source
       (origin
@@ -31,21 +53,23 @@
           (git-reference
             (url "https://github.com/rui314/mold")
             (commit (string-append "v" version))))
-        (file-name (git-file-name name version))
-        (sha256 (base32 "14dg32nbzmkc9z747qsaiy0x55kl00izzg1hsvcm31lpw3l1d2f8"))))
+        (sha256 (base32 "0ydgcx6vycg7jv4pksv4875cwqdysfm0cpscsdfy0inr5mjrbs8v"))))
     
     (build-system gnu-build-system)
     (arguments
-      '(#:make-flags
-         (list (string-append "CXX=" (assoc-ref %build-inputs "gcc") "/bin/g++")
-               (string-append "CC=" (assoc-ref %build-inputs "gcc") "/bin/gcc"))
+      `(#:make-flags
+         (list (string-append "CXX=" ,(cxx-for-target))
+               (string-append "CC=" ,(cc-for-target))
+               "SYSTEM_MIMALLOC=1" "SYSTEM_TBB=1" "SYSTEM_XXHASH=1")
         #:phases
           (modify-phases %standard-phases
             (delete 'configure)
             (replace 'install
               (lambda* (#:key outputs #:allow-other-keys)
-                (let* ((out (assoc-ref outputs "out")) (bin (string-append out "/bin")))
-                  (install-file "mold" bin)))))
+                (let* ((out (assoc-ref outputs "out")))
+                  (invoke "make" "SYSTEM_MIMALLOC=1" "SYSTEM_TBB=1" "SYSTEM_XXHASH=1" (string-append "PREFIX=" out) "install")))))
+        ; TODO: enable tests once $CC is respected
+        ; see: https://github.com/rui314/mold/commit/4324cb29988edf6cd7029d403afd09cac9abf0f3
         #:tests? #f))))
 
 mold
